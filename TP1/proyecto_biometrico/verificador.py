74738e6d80168bf14320522251416e65c4903686
Martin-NT <martinnavarro1503@gmail.com>
2025-07-21 18:57:22 -0300

    Tarea 2 - Puntos 2,3y4

--- TP1/proyecto_biometrico/verificador.py
+++ TP1/proyecto_biometrico/verificador.py
@@ -1,7 +1,19 @@
+import hashlib
+import json
+
 class Verificador:
     def __init__(self, conn):
         self.conn = conn
         self.resultados_por_timestamp = {}
+        self.chain = [] # Aca se almacena la cadena de bloques en memoria
+
+    def calcular_hash(self, prev_hash, datos, timestamp):
+        to_hash = prev_hash + str(datos) + timestamp
+        return hashlib.sha256(to_hash.encode()).hexdigest()
+    
+    def guardar_cadena(self):
+        with open("blockchain.json", "w") as f:
+            json.dump(self.chain, f, indent=4)
 
     def verificar_alerta(self, freq_media, oxi_media, presion_media):
         if freq_media >= 200:
@@ -13,7 +25,7 @@ class Verificador:
         else:
             return False
 
-    def verificar(self): 
+    def verificar(self):
         try:
             while True:
                 try:
@@ -37,11 +49,27 @@ class Verificador:
                     presion_media = datos_completos["presion"]["media"]
 
                     alerta = self.verificar_alerta(frecuencia_media, oxigeno_media, presion_media)
+                    
+                    prev_hash = self.chain[-1]["hash"] if self.chain else "0"*64
+                    hash_actual = self.calcular_hash(prev_hash, datos_completos, ts)
+                    
+                    bloque = {
+                        "timestamp": ts,
+                        "datos": datos_completos,
+                        "alerta": alerta,
+                        "prev_hash": prev_hash,
+                        "hash": hash_actual
+                    }
+
+                    self.chain.append(bloque)
+                    self.guardar_cadena()
 
                     print(f"[VERIFICADOR] Datos completos para el timestamp {ts}: \n{datos_completos}")
                     print(f"[VERIFICADOR] Alerta: {alerta}")
+                    print(f"[VERIFICADOR] Bloque #{len(self.chain)-1} - Hash: {hash_actual} - Alerta: {alerta}")
 
         except KeyboardInterrupt:
             print("[VERIFICADOR] Interrumpido por el usuario. Cerrando proceso.")
+            self.guardar_cadena()
         finally:
             self.conn.close()
